22e840684d335c89318991ba295ef7bfc8bf2058
Tsiry <hei.tsiry@gmail.com>
2026-01-17 20:07:54 +0300

    add DishIngredient

---
+++ src/main/java/org/tsiry/DishIngredient.java
@@ -0,0 +1,60 @@
+package org.tsiry;
+
+public class DishIngredient {
+    private Integer id;
+    private Dish dish;
+    private Ingredient ingredient;
+    private Double quantityRequired;
+    private UnitType unit;
+
+    public DishIngredient() {
+    }
+
+    public DishIngredient(Integer id, Dish dish, Ingredient ingredient, Double quantityRequired, UnitType unit) {
+        this.id = id;
+        this.dish = dish;
+        this.ingredient = ingredient;
+        this.quantityRequired = quantityRequired;
+        this.unit = unit;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Dish getDish() {
+        return dish;
+    }
+
+    public void setDish(Dish dish) {
+        this.dish = dish;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Double getQuantityRequired() {
+        return quantityRequired;
+    }
+
+    public void setQuantityRequired(Double quantityRequired) {
+        this.quantityRequired = quantityRequired;
+    }
+
+    public UnitType getUnit() {
+        return unit;
+    }
+
+    public void setUnit(UnitType unit) {
+        this.unit = unit;
+    }
+}
---
+++ src/main/java/org/tsiry/UnitType.java
@@ -0,0 +1,5 @@
+package org.tsiry;
+
+public enum UnitType {
+    PCS, KG, L
+}
